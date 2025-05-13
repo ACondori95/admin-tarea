34973a4eb564c1735fa9687e802acb39cedcf251
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-13 11:02:05 -0300

    Defining Task MongoDB Schema

---
+++ backend/models/Tarea.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const pendienteSchema = new mongoose.Schema({
+  text: {type: String, required: true},
+  completed: {type: Boolean, default: false},
+});
+
+const TareaSchema = new mongoose.Schema(
+  {
+    title: {type: String, required: true},
+    description: {type: String},
+    priority: {type: String, enum: ["Baja", "Media", "Alta"], default: "Media"},
+    status: {
+      type: String,
+      enum: ["Pendiente", "En Progreso", "Completeda"],
+      default: "Pendiente",
+    },
+    dueDate: {type: Date, required: true},
+    assignedTo: [{type: mongoose.Schema.Types.ObjectId, ref: "Usuario"}],
+    createdBy: {type: mongoose.Schema.Types.ObjectId, ref: "Usuario"},
+    attachments: [{type: String}],
+    todoChecklist: [pendienteSchema],
+    progress: {type: Number, default: 0},
+  },
+  {timestamps: true}
+);
+
+module.exports = mongoose.model("Tarea", TareaSchema);
