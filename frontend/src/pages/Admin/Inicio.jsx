d446c0170c40497bbedb277fb0718678f0b987b1
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-14 17:58:58 -0300

    Creating the Dashboard Page Layout

--- frontend/src/pages/Admin/Inicio.jsx
+++ frontend/src/pages/Admin/Inicio.jsx
@@ -1,11 +1,14 @@
 import React, {useContext} from "react";
 import {useUserAuth} from "../../hooks/useUserAuth";
 import {UserContext} from "../../context/UserContext";
+import DashboardLayout from "../../components/Layouts/DashboardLayout";
 
 const Inicio = () => {
   useUserAuth();
 
-  return <div>Inicio</div>;
+  const {user} = useContext(UserContext);
+
+  return <DashboardLayout activeMenu='Inicio'>Inicio</DashboardLayout>;
 };
 
 export default Inicio;
---
+++ frontend/src/utils/data.js
@@ -0,0 +1,48 @@
+import {
+  LuClipboardCheck,
+  LuLayoutDashboard,
+  LuLogOut,
+  LuSquarePlus,
+  LuUsers,
+} from "react-icons/lu";
+
+export const SIDE_MENU_DATA = [
+  {id: "01", label: "Inicio", icon: LuLayoutDashboard, path: "/admin/inicio"},
+  {
+    id: "02",
+    label: "Administrar Tareas",
+    icon: LuClipboardCheck,
+    path: "/admin/tareas",
+  },
+  {
+    id: "03",
+    label: "Crear Tarea",
+    icon: LuSquarePlus,
+    path: "/admin/crear-tareas",
+  },
+  {id: "04", label: "Miembros del Equipo", icon: LuUsers, path: "/admin/users"},
+  {id: "05", label: "Salir", icon: LuLogOut, path: "salir"},
+];
+
+export const SIDE_MENU_USER_DATA = [
+  {id: "01", label: "Inicio", icon: LuLayoutDashboard, path: "/usuario/inicio"},
+  {
+    id: "02",
+    label: "Mis Tareas",
+    icon: LuClipboardCheck,
+    path: "/usuario/mis-tareas",
+  },
+  {id: "05", label: "Salir", icon: LuLogOut, path: "salir"},
+];
+
+export const PRIORITY_DATA = [
+  {label: "Baja", value: "Baja"},
+  {label: "Media", value: "Media"},
+  {label: "Alta", value: "Alta"},
+];
+
+export const STATUS_DATA = [
+  {label: "Pendiente", value: "Pendiente"},
+  {label: "En Progreso", value: "En Progreso"},
+  {label: "Completada", value: "Completada"},
+];
