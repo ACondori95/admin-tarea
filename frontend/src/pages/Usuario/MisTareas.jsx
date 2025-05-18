959c8bb275695fa1c92137350ef0f863df1a393d
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-18 20:59:10 -0300

    My Tasks Page UI

--- frontend/src/pages/Usuario/MisTareas.jsx
+++ frontend/src/pages/Usuario/MisTareas.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import DashboardLayout from "../../components/Layouts/DashboardLayout";
+import {useNavigate} from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import {API_PATHS} from "../../utils/apiPaths";
+import TaskStatusTabs from "../../components/TaskStatusTabs";
+import TaskCard from "../../components/Cards/TaskCard";
 
 const MisTareas = () => {
-  return <div>MisTareas</div>;
+  const [allTasks, setAllTasks] = useState([]);
+
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("Todo");
+
+  const navigate = useNavigate();
+
+  const getAllTasks = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {status: filterStatus === "Todo" ? "" : filterStatus},
+      });
+
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      // Map statusSummary data with fixed labels and order
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        {label: "Todo", count: statusSummary.all || 0},
+        {label: "Pendiente", count: statusSummary.pendingTasks || 0},
+        {label: "En Progreso", count: statusSummary.inProgressTasks || 0},
+        {label: "Completada", count: statusSummary.completedTasks || 0},
+      ];
+
+      setTabs(statusArray);
+    } catch (error) {
+      console.error("Error al obtener los usuarios:", error);
+    }
+  };
+
+  const handleClick = (taskId) => {
+    navigate(`/usuario/ver-detalles/${taskId}`);
+  };
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+    return () => {};
+  }, [filterStatus]);
+
+  return (
+    <DashboardLayout activeMenu='Mis Tareas'>
+      <div className='my-5'>
+        <div className='flex flex-col lg:flex-row lg:items-center justify-between'>
+          <h2 className='text-xl md:text-xl font-medium'>Mis Tareas</h2>
+
+          {tabs?.[0]?.count > 0 && (
+            <TaskStatusTabs
+              tabs={tabs}
+              activeTab={filterStatus}
+              setActiveTab={setFilterStatus}
+            />
+          )}
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+          {allTasks?.map((item, index) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => {
+                handleClick(item._id);
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default MisTareas;
